22951c482c1281f40e365011eaad45e57350b586
tenkoh <tenkoh.go@gmail.com>
2022-05-01 13:25:01 +0900

    wip: add code generate tool, but found some fix is needed to calculate for replacement

--- textstyle_test.go
+++ textstyle_test.go
@@ -2,7 +2,6 @@ package textstyle_test
 
 import (
 	"io"
-	"os"
 	"strings"
 	"testing"
 
@@ -11,10 +10,25 @@ import (
 )
 
 func ExampleTransformer() {
-	s := "Hello, Gophers"
-	r := transform.NewReader(strings.NewReader(s), textstyle.Bold)
-	io.Copy(os.Stdout, r)
+	// s := "Hello, Gophers"
+	// r := transform.NewReader(strings.NewReader(s), textstyle.Bold)
+	// io.Copy(os.Stdout, r)
 	// Output: 𝐇𝐞𝐥𝐥𝐨, 𝐆𝐨𝐩𝐡𝐞𝐫𝐬
+}
+
+func TestItalic(t *testing.T) {
+	src := "My new gear..."
+	want := "𝑀𝑦 𝑛𝑒𝑤 𝑔𝑒𝑎𝑟..."
+
+	reader := transform.NewReader(strings.NewReader(src), textstyle.Italic)
+	b, err := io.ReadAll(reader)
+	if err != nil {
+		t.Errorf("got error: %s\n", err)
+		return
+	}
+	if got := string(b); got != want {
+		t.Errorf("want %s, got %s\n", want, got)
+	}
 }
 
 func TestBold(t *testing.T) {
